19f9a3207b709b1a1239b358f29b79a4cfa6e3d0
Deight <d8corp@mail.ru>
2026-03-23 18:17:19 +0300

    First implementation

---
+++ src/jsx-dev-runtime.ts
@@ -0,0 +1,4 @@
+import { renderJSXDev } from '@innet/jsx'
+
+export const jsxDEV = renderJSXDev
+export const Fragment = undefined
---
+++ src/jsx-runtime.ts
@@ -0,0 +1,5 @@
+import { renderJSX } from '@innet/jsx'
+
+export const jsx = renderJSX
+export const jsxs = renderJSX
+export const Fragment = undefined
